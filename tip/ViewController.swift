d21e4c6e795296208e9f4bd0fac6c3d4d7dc8196
Maria <maria.cordero6@upr.edu>
2020-08-02 01:56:56 -0400

    Finished base requirements, changed keyboard for numpad, numpad is now permanent

--- tip/ViewController.swift
+++ tip/ViewController.swift
@@ -10,11 +10,35 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var totalLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var tipControl: UISegmentedControl!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        billField.becomeFirstResponder()
         // Do any additional setup after loading the view.
     }
+//
+    @IBAction func onTap(_ sender: Any) {
+    }
+    
+    @IBAction func calculateTip(_ sender: Any) {
+        //Get bill amount
+        let bill = Double(billField.text!) ?? 0
+        
+        
+        //Tip and total amount
+        let tipPercentages = [0.15,0.18,0.20]
+        let tip = bill * tipPercentages[tipControl.selectedSegmentIndex]
+        let total = bill + tip
+        
+        //Update tip and total label
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLabel.text = String(format: "$%.2f", total)
 
-
+    }
+    
 }
 
